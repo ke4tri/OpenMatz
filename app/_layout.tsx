0c4fb971f0ac1eb6daa6d6a992d6bd4ff1df0754
Wayne Collier <ke4tri@gmail.com>
2025-06-07 10:58:27 -0500

    updated orientation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 // app/_layout.tsx
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function Layout() {
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+  }, []);
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
